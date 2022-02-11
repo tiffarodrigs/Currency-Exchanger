5c824a8ca50564efdfb15eeeff1dcef0e9c277a2
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-11 12:21:38 -0800

    WIP- update to handle error

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,9 +1,10 @@
 export default class Currency{
   static currencyExchange(srcCurrency){
+    console.log(process.env.API_KEY)
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${srcCurrency}`)
       .then(function(response){
         if(!response.ok){
-          throw Error(response.statusText);
+          throw Error(response["error-type"]);
         }
         return response.json();
       })
